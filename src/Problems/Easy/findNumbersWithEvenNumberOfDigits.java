5b503d2cfc942855c2202f6c11b3eb0a493044b3
Jasminoza <testbot91@gmail.com>
2022-03-19 13:50:48 +0300

    One more task solved

---
+++ src/Problems/Easy/findNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,22 @@
+package Problems.Easy;
+
+public class findNumbersWithEvenNumberOfDigits {
+
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(findNumbers(nums)); //2
+        int[] nums2 = {555, 901, 482, 1771};
+        System.out.println(findNumbers(nums2)); //1
+    }
+
+    public static int findNumbers(int[] nums) {
+        int evenCount = 0;
+        for (int num : nums) {
+            String numStr = num + "";
+            if (numStr.length() % 2 == 0) {
+                evenCount++;
+            }
+        }
+        return evenCount;
+    }
+}
