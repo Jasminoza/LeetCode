aada9a7cc0019ac5c87ca68692a71ed68a89697d
Jasminoza <testbot91@gmail.com>
2022-03-31 16:35:57 +0300

    One more task solved. Damn that's genius.

---
+++ src/Problems/Arrays/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,62 @@
+package Problems.Arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.SortedMap;
+
+/**
+ * Given an array arr, replace every element in that array with the greatest
+ * element among the elements to its right, and replace the last element with -1.
+ * <p>
+ * After doing so, return the array.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: arr = [17,18,5,4,6,1]
+ * Output: [18,6,6,6,1,-1]
+ * Explanation:
+ * - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+ * - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+ * - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+ * - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+ * - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+ * - index 5 --> there are no elements to the right of index 5, so we put -1.
+ * Example 2:
+ * <p>
+ * Input: arr = [400]
+ * Output: [-1]
+ * Explanation: There are no elements to the right of index 0.
+ */
+
+public class ReplaceElementsWithGreatestElementOnRightSide {
+    public static void main(String[] args) {
+        int[] arr = {17, 18, 5, 4, 6, 1};
+        System.out.println(Arrays.toString(replaceElements(arr))); // [18,6,6,6,1,-1]
+        int[] arr2 = {400};
+        System.out.println(Arrays.toString(replaceElements(arr2))); // [-1]
+    }
+
+    public static int[] replaceElements(int[] arr) {
+
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+        int max = arr[arr.length - 1];
+        int temp;
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] < max) {
+                arr[i] = max;
+            } else {
+                temp = max;
+                max = arr[i];
+                arr[i] = temp;
+            }
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+}
