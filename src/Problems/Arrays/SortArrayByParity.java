15d582dd67fb2024621def796c58a9d4c7ceeacf
Jasminoza <testbot91@gmail.com>
2022-03-31 17:34:03 +0300

    One more task solved.

---
+++ src/Problems/Arrays/SortArrayByParity.java
@@ -0,0 +1,30 @@
+package Problems.Arrays;
+
+import java.util.Arrays;
+
+public class SortArrayByParity {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 2, 4};
+        System.out.println(Arrays.toString(sortArrayByParity(nums))); //2,4,3,1. The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+    }
+
+    public static int[] sortArrayByParity(int[] nums) {
+        int[] arr = new int[nums.length];
+        int counter = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                arr[counter] = num;
+                counter++;
+            }
+        }
+
+        for (int num : nums)
+            if (num % 2 != 0) {
+                arr[counter] = num;
+                counter++;
+            }
+
+        return arr;
+    }
+}
